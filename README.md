a7aad3342549ee4160850ec4470a3e030273032d
Rajat Harlalka <rajatharlalka@Rajats-MacBook-Pro.local>
2017-08-14 18:12:47 +0530

    Added app functionality

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# rblog
+RBlog
+
+A complete blog management system like WordPress in Ruby on Rails.
+
+Features:
+- Full featured blogging systen
+- Posts with categories
+- Commenting system
+- Backend admin area
+- Custom user management system
+- Login
+- RESTful routing
+
+Technologies
+Datase: SQL3Lite
+
---
+++ config/routes.rb
@@ -0,0 +1,181 @@
+Rails.application.routes.draw do
+  get 'contact' => 'contacts#new'
+
+  get 'contacts/create'
+
+  get 'about' => 'about#index'
+
+  namespace :admin do
+  get 'login' => 'sessions#new', :as => 'login'
+  end
+
+  namespace :admin do
+    get 'logout' => 'sessions#destroy', :as => 'logout'
+  end
+
+  namespace :admin do
+  get 'sessions/create'
+  end
+
+  namespace :admin do
+  get 'users/new'
+  end
+
+  namespace :admin do
+  get 'users/create'
+  end
+
+  namespace :admin do
+  get 'users/edit'
+  end
+
+  namespace :admin do
+  get 'users/update'
+  end
+
+  namespace :admin do
+  get 'users/destroy'
+  end
+
+  namespace :admin do
+  get 'users/index'
+  end
+
+  namespace :admin do
+  get 'users/show'
+  end
+
+  namespace :admin do
+  get 'comments/destroy'
+  end
+
+  get 'comments/new'
+
+  namespace :admin do
+  get 'categories/new'
+  end
+
+  namespace :admin do
+  get 'categories/create'
+  end
+
+  namespace :admin do
+  get 'categories/edit'
+  end
+
+  namespace :admin do
+  get 'categories/update'
+  end
+
+  namespace :admin do
+  get 'categories/destroy'
+  end
+
+  namespace :admin do
+  get 'categories/index'
+  end
+
+  namespace :admin do
+  get 'categories/show'
+  end
+
+  get 'categories/show'
+
+  namespace :admin do
+  get 'posts/new'
+  end
+
+  namespace :admin do
+  get 'posts/create'
+  end
+
+  namespace :admin do
+  get 'posts/edit'
+  end
+
+  namespace :admin do
+  get 'posts/update'
+  end
+
+  namespace :admin do
+  get 'posts/destroy'
+  end
+
+  namespace :admin do
+  get 'posts/index'
+  end
+
+  namespace :admin do
+  get 'posts/show'
+  end
+
+  get 'posts/index'
+
+  get 'posts/show'
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+  root 'posts#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+  resources :posts, :categories, :comments
+  resources "contacts", only: [:new, :create]
+
+  namespace :admin do
+    resources :posts, :categories, :comments, :users, :sessions
+  end
+
+  resources :posts do
+    resources :comments
+  end
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
